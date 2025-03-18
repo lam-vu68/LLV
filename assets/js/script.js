0ce89f1e8f9f63131d0e97d48c280b70176cb24f
Vu oi <vuly62626@gmail.com>
2025-03-18 10:30:14 +0700

    gần xong

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  initialSlide: 2,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
